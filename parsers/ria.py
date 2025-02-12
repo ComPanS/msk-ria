92a32c9f906b02897f056988362abc45c8763730
ComPanS <nazmiev2006@gmail.com>
2025-02-12 18:34:45 +0300

    s

--- parsers/ria.py
+++ parsers/ria.py
@@ -37,8 +37,10 @@ async def parse_page(url):
             # Попробуем найти изображение
             image_url = None
             image_div_locator = page.locator("div.media__size img")
+
+            # Увеличиваем время ожидания для изображения (например, до 90 секунд)
             try:
-                await image_div_locator.wait_for(timeout=60000)  # 90 секунд для загрузки изображения
+                await image_div_locator.wait_for(timeout=90000)  # 90 секунд для загрузки изображения
                 if await image_div_locator.count() > 0:  # Проверяем, есть ли изображение
                     image_url = await image_div_locator.get_attribute("src")  # Получаем URL изображения
             except Exception as e:
